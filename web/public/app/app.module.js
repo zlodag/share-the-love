a912eff4fc76f28a07dcd8a95dc023dfd65341c3
zlodag <zlodag@users.noreply.github.com>
2016-10-31 19:25:35 +1300

    removed broken listener

--- web/public/app/app.module.js
+++ web/public/app/app.module.js
@@ -1,15 +1,25 @@
 "use strict";
 (function() {
     angular
-        .module("shareTheLove", ["firebase","ngMessages","ui.router"])
-        .run(function($rootScope){
-			$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, error) {
-	            console.log("Starting");
-				if(toState.name == 'Categories') {
-				// Do Something
-				}
-			});
-		});
+        .module("shareTheLove", ["firebase","ngMessages","ui.router"]);
+  //       .run(["$rootScope", function($rootScope){
+  //           console.log($rootScope.$on);
+  //           $rootScope.$on('$locationChangeSuccess', function(event, toState, toParams, fromState, fromParams, error){
+  //               console.log("fat chance");
+  //           });
+  //           $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams, error){
+  //               console.log("fatter chance");
+  //           });
+  //           $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){
+  //               console.log("error chance");
+  //           });
+		// 	$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, error) {
+	 //            console.log("Starting");
+		// 		if(toState.name == 'Categories') {
+		// 		// Do Something
+		// 		}
+		// 	});
+		// }])
     	// .run(["$rootScope", "$state", catchSignInError]);
 
     // function catchSignInError($rootScope, $state) {
